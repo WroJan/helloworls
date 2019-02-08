7124af47bae1fe9fcc864c2c3f046877a5e42b07
WroJan <janek93.wroblewski@gmail.com>
2019-02-08 16:42:52 +0000

    etst

---
+++ app/src/main/java/wit/org/MainActivity.kt
@@ -0,0 +1,19 @@
+package wit.org
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        
+
+
+
+
+
+    }
+}
